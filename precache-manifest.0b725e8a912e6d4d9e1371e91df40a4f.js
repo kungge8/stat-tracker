1027610dcb725191302bd80e61adb9ef57fcc731
Kevin <leeyhteachi@gmail.com>
2020-02-21 12:26:08 -0500

    Updates

---
+++ precache-manifest.0b725e8a912e6d4d9e1371e91df40a4f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a4171b26a5763c75957c3b540862b34",
+    "url": "/stat-tracker/index.html"
+  },
+  {
+    "revision": "660fff7e83ce85c2f115",
+    "url": "/stat-tracker/static/css/main.871bc758.chunk.css"
+  },
+  {
+    "revision": "415641d2a2a88334e2c4",
+    "url": "/stat-tracker/static/js/2.1516882f.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/stat-tracker/static/js/2.1516882f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "660fff7e83ce85c2f115",
+    "url": "/stat-tracker/static/js/main.153dff1b.chunk.js"
+  },
+  {
+    "revision": "0b1a4e54fdefed9d0506",
+    "url": "/stat-tracker/static/js/runtime-main.64e49256.js"
+  }
+]);
